a64df38327c0c57d7eb0da0b0a58c76b0a5b801c
yuhyeonjo <jo.yuhyeon16@gmail.com>
2023-06-13 16:13:05 +0900

    정보은닉 : 접근제어자(private 변수 사용)

--- src/hiding/MyDate.java
+++ src/hiding/MyDate.java
@@ -2,7 +2,48 @@ package hiding;
 
 public class MyDate {
 
-    int day;
-    int month;
-    int year;
+    private int day;
+    private int month;
+    private int year;
+
+    private boolean isValid = true;
+
+
+    /* 만약 getter 만 만들 경우 readonly : 처음 생성하고 변경하면 안되는 값 */
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        if( month < 1 || month > 12) {
+            isValid = false;
+        } else this.month = month;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+
+
+    public void showDate() {
+        if(isValid){
+            System.out.println( year + "년" + month + "월" + day + "일 입니다.");
+        } else {
+            System.out.println("유효하지 않은 날짜 입니다.");
+        }
+    }
 }
--- src/hiding/MyDateTest.java
+++ src/hiding/MyDateTest.java
@@ -4,5 +4,22 @@ public class MyDateTest {
 
     public static void main(String[] args) {
 
+        MyDate date = new MyDate();
+
+    /*
+        private 접근제어자로 변경 시 오류
+        private 변수 public get/set 메서드를 이용하여 값 가져오기
+
+        date.day = 13;
+        date.month = 6;
+        date.year = 2023;
+
+     */
+
+        date.setDay(13);
+        date.setMonth(6);
+        date.setYear(2023);
+
+        date.showDate();
     }
 }
